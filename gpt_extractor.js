47491f9bfda6686d2829084d1e223dc0690c1342
Mojahid islam <raofahmedmojahid@gmail.com>
2024-05-19 13:08:02 +0600

    inisial

---
+++ gpt_extractor.js
@@ -0,0 +1,7 @@
+"use strict";
+
+let show_gpt_alert = document.getElementById("show_gpt_alert");
+show_gpt_alert.addEventListener("click", function () {
+    alert("You clicked the button!");
+
+});
